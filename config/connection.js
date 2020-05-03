ee081b2cfb711a005ba4e0a49fc7f03bf3bd9a3d
cshanebrown78 <cshanebrown78@gmail.com>
2020-05-02 22:28:24 -0500

    added var in connection file

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // MySQL connection setup.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection ({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "A!rplane1",
     database: "burgers_db"
 });
+};
 
 // Makes the connection
 connection.connect(function(err) {
